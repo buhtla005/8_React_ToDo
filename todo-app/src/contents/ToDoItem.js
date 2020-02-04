466e8a1dcc18905d8821cb5ac00d32213fb79d9e
unknown <noobprogramer@yahoo.com>
2020-02-05 00:53:13 +0100

    Second commit. Updated version.

--- todo-app/src/contents/ToDoItem.js
+++ todo-app/src/contents/ToDoItem.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
 function ToDoItem(props){
+
+    const completedStyle = {
+        fontstyle: "italic",
+        color: "#cdcdcd",
+        textDecoration: "line-through"
+    }
+
     return(
         //checked svojstvo se isto moze sredit kao fun
         <div className="todo-item">
@@ -9,7 +16,7 @@ function ToDoItem(props){
             checked={props.todo.completed}
             onClick={()=>props.changeClickedState(props.todo.id)}
             className="col-4"/>
-            <h3>Zadatak {props.todo.id}: {props.todo.text}</h3>
+            <h3 style={props.todo.completed ? completedStyle : null}>Zadatak {props.todo.id}: {props.todo.text}</h3>
         </div>
     )
 }
